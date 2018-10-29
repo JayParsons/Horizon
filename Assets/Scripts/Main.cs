775aad4e0fdec2ed14305da05764acee883213dd
JayParsons <jason.parsons@dal.ca>
2018-10-29 11:25:22 -0300

    First commit. Currently game is just a prototype of a basic shape flying through a starfield.

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    public GameObject star;
+
+	// Use this for initialization
+	void Start () {
+        CreateStarfield();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void CreateStarfield()
+    {
+        for (int i = 0; i < 20000; i++)
+        {
+            //set position of new star randomly
+            float x = Random.Range(-200, 200);
+            float y = Random.Range(-200, 200);
+            float z = Random.Range(-200, 200);
+
+            //place star in scene
+            Instantiate(star);
+            Vector3 pos;
+            pos.x = x;
+            pos.y = y;
+            pos.z = z;
+            star.transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/ShipControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //ship moves forward a little by default
+        GetComponent<Rigidbody>().AddForce(transform.forward * Time.deltaTime * 300);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Pressing up arrow");
+            GetComponent<Rigidbody>().AddForce(transform.forward * Time.deltaTime * 600);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Pressing down arrow");
+            GetComponent<Rigidbody>().AddForce(-transform.forward * Time.deltaTime * 600);
+        }
+
+        //Pressing 'A' or 'D' allows the player and the camera to be roated side to side
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressing A key");
+            transform.RotateAround(GetComponent<Rigidbody>().position, Vector3.up, -1);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressing D key");
+            transform.RotateAround(GetComponent<Rigidbody>().position, Vector3.up, 1);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Pressing W key");
+            transform.RotateAround(GetComponent<Rigidbody>().position, Vector3.right, -1);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Pressing S key");
+            transform.RotateAround(GetComponent<Rigidbody>().position, Vector3.left, -1);
+        }
+
+    }
+}
